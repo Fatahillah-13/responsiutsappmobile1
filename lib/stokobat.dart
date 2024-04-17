198962e907e19ef8f416ac34b2751feadf685351
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2024-04-17 18:25:50 +0700

    create page stokobat mins bottomButton

--- lib/stokobat.dart
+++ lib/stokobat.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,8 +17,115 @@ class Stokobat extends StatelessWidget {
             'Stok Obat',
             style: TextStyle(
               color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
           ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Container(
+                width: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.grey,
+                ),
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 50,
+                          margin: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.pink[400]),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Pinisilin',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                width: 150,
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.more_vert),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          width: 20,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Fatah Apotik',
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              'A12.2020.06454',
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              'Menjual Berbagai Jenis Obat',
+                              textAlign: TextAlign.start,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 70,
+                          height: 20,
+                        ),
+                        Column(
+                          children: [
+                            Image.asset(
+                              'assets/logo.png',
+                              width: 100,
+                              height: 200,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(),
+          ],
         ),
       ),
     );
